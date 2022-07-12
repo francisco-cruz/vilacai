018f472c813074d5fc1c6dd4a4aba2b86590d002
francisco-cruz <franaraujosilva2017@gmail.com>
2022-07-12 16:06:13 -0300

    colocando data-* nos produtos

--- scripts/modalAdicionarRecheio.js
+++ scripts/modalAdicionarRecheio.js
@@ -1,37 +1,59 @@
-function abrirModalAdicionar(){
 
-    let modal = document.querySelector('.modaladicionar')
-    modal.style.display = 'block';
-  }
-  
-  function fecharModalAdicionar(){
-    let modal = document.querySelector('.modaladicionar')
-    modal.style.display = 'none';
-  }
+function abrirModalAdicionar() {
+
+  let modal = document.querySelector('.modaladicionar')
+  modal.style.display = 'block';
+
+}
+
+function fecharModalAdicionar() {
+  let modal = document.querySelector('.modaladicionar')
+  modal.style.display = 'none';
+}
+
+
+// pegando as informações e colocando no modal
+function pegarInfoModal(produto) {
+  let secao = produto.getAttribute('data-secao');
+  let nome = produto.getAttribute('data-nome');
+  let preco = produto.getAttribute('data-preco');
+
+  let modalTitulo = document.querySelector('.titulomodal');
+  let modalNome = document.querySelector('#produtomodal');
+  let modalPreco = document.querySelector('#precomodal');
+ 
+  modalTitulo.innerText = secao;
+  modalNome.innerText = nome;
+  modalPreco.innerText = "R$ " + preco;
+
+  abrirModalAdicionar();
+}
+
+
 
 // eventos do botão quantidade
 
-let qtd2 = document.querySelector('#qtd').innerHTML;
-let qtdReal = parseInt(qtd2)
+let quantidade = document.querySelector('#qtd').innerHTML;
+let quantidadeReal = parseInt(quantidade)
 
 
-  function diminuirQuantidade(){    
-    if(qtdReal === 1){
-      document.querySelector('#iconAddMenos').off('click');
-    }else {
-      qtdReal -=1;
-      document.querySelector("#qtd").innerText= qtdReal;
-}  
-
-
+function diminuirQuantidade() {
+  if (quantidadeReal === 1) {
+    document.querySelector('#iconAddMenos').off('click');
+  } else {
+    quantidadeReal -= 1;
+    document.querySelector("#qtd").innerText = quantidadeReal;
   }
 
-  function aumentarQuantidade(){
-    if(qtdReal === 10){
-      document.querySelector('#iconAddMais').off('click');
-    }else {
-      qtdReal +=1;
-      document.querySelector("#qtd").innerText= qtdReal;
+
 }
-  
+
+function aumentarQuantidade() {
+  if (quantidadeReal=== 10) {
+    document.querySelector('#iconAddMais').off('click');
+  } else {
+    quantidadeReal += 1;
+    document.querySelector("#qtd").innerText = quantidadeReal;
   }
+
+}
