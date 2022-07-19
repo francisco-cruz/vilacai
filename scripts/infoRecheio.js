9b15a0d01f5c27b0f539d2bd1c2a5a0bf446dc43
francisco-cruz <franaraujosilva2017@gmail.com>
2022-07-19 11:29:31 -0300

    criando scripts dos recheios

--- scripts/infoRecheio.js
+++ scripts/infoRecheio.js
@@ -1,9 +1,44 @@
+
 function pegarInfoRecheio(produto) {
-    let infoProduto = document.querySelector('.produtoInfo');
-    let nome = produto.getAttribute('data-nome');
-    console.log(nome);
-    infoProduto.innerText = nome; 
-    console.log(nome);
+  let nome = sessionStorage.getItem('nome');
+  let infoProduto = document.querySelector('.produtoInfo');
+  infoProduto.innerText = nome; 
+   
   }
- 
-  pegarInfoRecheio(produto);
+  pegarInfoRecheio();
+
+  function quantidadeRecheio (produto){
+    let quantidade = sessionStorage.getItem('quantidade-de-recheio');
+    let qtdTotal = document.querySelector('.qtd-total-recheio');
+    qtdTotal.innerText = quantidade; 
+
+
+    
+    let qtdSoma = document.querySelector('.contador-recheio');
+    qtdSoma.innerText = quantidade; 
+
+  }
+  quantidadeRecheio();
+
+
+  let quantidade = document.querySelector('#contador-recheio').innerHTML;
+  let quantidadeReal = parseInt(quantidade)
+
+    // diminuir quantidade
+function diminuirQuantidade() {
+  if (quantidadeReal === 1) {
+    document.querySelector('#iconAddMenos').off('click');
+  } else {
+    quantidadeReal -= 1;
+    document.querySelector("#qtd").innerText = quantidadeReal;
+  }
+}
+    // aumentar quatidade
+function aumentarQuantidade() {
+  if (quantidadeReal=== 10) {
+    document.querySelector('#iconAddMais').off('click');
+  } else {
+    quantidadeReal += 1;
+    document.querySelector("#qtd").innerText = quantidadeReal;
+  }
+}
