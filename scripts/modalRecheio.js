fa6864145e6319ac8c968ca523d9edb90f42ab21
francisco-cruz <franaraujosilva2017@gmail.com>
2022-07-20 17:10:25 -0300

    finalizando modal recheio

---
+++ scripts/modalRecheio.js
@@ -0,0 +1,14 @@
+function openModalRecheio() {
+    let modal = document.querySelector('.bg-modal-recheio');
+    modal.style.display = 'flex';
+    showInfo();
+ }
+ 
+ function closedModalRecheio() {
+    let modal = document.querySelector('.bg-modal-recheio');
+    modal.style.display = 'none';
+ }
+
+ function showInfo(produto){
+ 
+ }
---
+++ scripts/modalStore.js
@@ -0,0 +1,9 @@
+function openModal() {
+   let modal = document.querySelector('.bg-modal-store');
+   modal.style.display = 'flex';
+}
+
+function closedModal() {
+   let modal = document.querySelector('.bg-modal-store');
+   modal.style.display = 'none';
+}
