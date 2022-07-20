d0ba4902b1411773e2668e9c7d7646d1652e39ae
francisco-cruz <franaraujosilva2017@gmail.com>
2022-07-20 14:24:22 -0300

    refazendo  modal informações

--- scripts/pegarHora.js
+++ scripts/pegarHora.js
@@ -1,16 +1,11 @@
-// pegando horário e fazenedo condição
 function pegarHora(){
     const date = new Date().toLocaleTimeString();
-
     if (date <='18' || date > '22'){
-        let estado = document.getElementById("lbEstado");
-        estado.innerText="Fechado";
-        estado.className += " fechado";
-        estado.removeAttribute('id');
-
-    } else {
-        let estado = document.querySelectorById("lbEstado").innerText="Aberto";
-    }
+        let open = document.getElementById("store-status");
+        open.innerText="Fechado";
+        open.className += " closed";
+        open.removeAttribute('id');
+    } 
 }
 pegarHora();
 
