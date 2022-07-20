d88d3af396d2e19a5da322917a73c87c95038d6f
francisco-cruz <franaraujosilva2017@gmail.com>
2022-07-20 17:52:41 -0300

    tirando scroll da página ao abrir modal

--- scripts/modalRecheio.js
+++ scripts/modalRecheio.js
@@ -2,11 +2,17 @@ function openModalRecheio() {
     let modal = document.querySelector('.bg-modal-recheio');
     modal.style.display = 'flex';
     showInfo();
+   // tirar scroll da tela
+   let body = document.querySelector("html");
+   body.style.overflow = "hidden";
  }
  
  function closedModalRecheio() {
     let modal = document.querySelector('.bg-modal-recheio');
     modal.style.display = 'none';
+    // colocar scroll na tela
+   let body = document.querySelector("html");
+   body.style.overflow = "auto";
  }
 
  function showInfo(produto){
--- scripts/modalStore.js
+++ scripts/modalStore.js
@@ -1,9 +1,15 @@
 function openModal() {
    let modal = document.querySelector('.bg-modal-store');
    modal.style.display = 'flex';
+   // tirar scroll da tela
+   let body = document.querySelector("html");
+   body.style.overflow = "hidden";
 }
 
 function closedModal() {
    let modal = document.querySelector('.bg-modal-store');
    modal.style.display = 'none';
+   // colocar scroll na tela
+   let body = document.querySelector("html");
+   body.style.overflow = "auto";
 }
