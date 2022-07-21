d70f17360ebec15f50c5fb4d70e8cc7e4e083ef6
francisco-cruz <franaraujosilva2017@gmail.com>
2022-07-21 17:57:45 -0300

    revendo codigo

---
+++ scripts/test.js
@@ -0,0 +1,128 @@
+let products = [{
+        "acai-section": [{
+                "nome": "copo 900 ml",
+                "obs-product": "Até 8 adicionais",
+                "img": "./imagens/hamburgueira.jpeg",
+                "price": 20,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 400 ml",
+                "obs-product": "Até 10 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 600 ml",
+                "obs-product": "Até 15 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+        ]
+    },
+    {
+        "icecream-section": [{
+                "nome": "copo 900 ml",
+                "obs-product": "Até 8 adicionais",
+                "img": "./imagens/hamburgueira.jpeg",
+                "price": 20,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 400 ml",
+                "obs-product": "Até 10 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 600 ml",
+                "obs-product": "Até 15 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+        ]
+    },
+    {
+        "snaks-section": [{
+                "nome": "copo 900 ml",
+                "obs-product": "Até 8 adicionais",
+                "img": "./imagens/hamburgueira.jpeg",
+                "price": 20,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 400 ml",
+                "obs-product": "Até 10 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 600 ml",
+                "obs-product": "Até 15 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+        ]
+    },
+    {
+        "drinks-section": [{
+                "nome": "DRINK 900 ml",
+                "obs-product": "Até 8 adicionais",
+                "img": "./imagens/hamburgueira.jpeg",
+                "price": 20,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 400 ml",
+                "obs-product": "Até 10 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+            {
+                "nome": "copo 600 ml",
+                "obs-product": "Até 15 adicionais",
+                "img": "./imagens/copo.jpeg",
+                "price": 15,
+                "valor": 0,
+                "qntd": 0
+            },
+        ]
+    },
+]
+
+for (i in products) {
+    for (j in products[i]) // pega o tipo do produto
+        for (k in products[i][j]) {
+            let produto = '<div class="product" onclick="getInfo(this)">' +
+                '<img src="' + products[i][j][k]["img"] + '" alt="">' +
+                '<div class="info-product">' +
+                '<p class="name-product">' + products[i][j][k]["nome"] + '</p>' +
+                '<p class="obs-product">' + products[i][j][k]["obs-product"] + '</p>' +
+                '<p class="price-product">' + products[i][j][k]["price"] + '</p>' +
+                '</div>' +
+                '</div>';
+
+            document.getElementById(j).children[1].innerHTML += produto;
+
+            console.log(products[i][j][k]["nome"])
+        }
+
+}
